ab85a75c8b7b1f4832031c68212980f24c7f86ef
Areeb Bhaila <areeb.bhaila@gmail.com>
2020-11-11 16:49:52 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Delightful wooden house in rural Cotswold village',
+  address: 'Gloucestershire, England, United Kingdom',
+  description: 'Lovely peaceful place with a very cosy wood fire going. Well located for local walks through the nearby farms and a short drive to many quant little villages.',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Delightful wooden house in rural Cotswold village',
+  address: 'Gloucestershire, England, United Kingdom',
+  description: 'Lovely peaceful place with a very cosy wood fire going. Well located for local walks through the nearby farms and a short drive to many quant little villages.',
+  price_per_night: 75,
+  number_of_guests: 3
+)
